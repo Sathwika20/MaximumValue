70af7a98b063f3936979d2dc3d3db7efc90b8f7e
G.Sathwika <gopirajsathwika1999@gmail.com>
2021-03-10 21:54:13 +0530

    Added MaximumValue using Integer Added MaxValue using Float Added MaxValue using String

--- src/test/java/com/bridgelabz/MaxValueTest.java
+++ src/test/java/com/bridgelabz/MaxValueTest.java
@@ -34,14 +34,33 @@ public  class MaxValueTest {
     @Test
     public void when_second_Float_position_is_greater_should_return_true() {
         MaxValue maxValue = new MaxValue();
-        Float max = maxValue.givenMaxValue(2f,8f,3f);
-        Assert.assertEquals(8f,max,0f);
+        Float max = maxValue.givenMaxValue(2f,3f,1f);
+        Assert.assertEquals(3f,max,0f);
     }
     @Test
     public void when_third_Float_position_is_greater_should_return_true() {
         MaxValue maxValue = new MaxValue();
-        Float max = maxValue.givenMaxValue(2f,3f,9f);
-        Assert.assertEquals(9f,max,0f);
+        Float max = maxValue.givenMaxValue(3f,4f,5f);
+        Assert.assertEquals(5f,max,0f);
+    }
+    @Test
+    public void when_first_position_String_is_greater_should_return_true() {
+        MaxValue maxValue = new MaxValue();
+        String max = maxValue.givenMaxValue("red","green","orange");
+        Assert.assertEquals("red",max);
+    }
+
+    @Test
+    public void when_second_String_position_is_greater_should_return_true() {
+        MaxValue maxValue = new MaxValue();
+        String max = maxValue.givenMaxValue("pink","white","purple");
+        Assert.assertEquals("white",max);
+    }
+    @Test
+    public void when_third_String_position_is_greater_should_return_true() {
+        MaxValue maxValue = new MaxValue();
+        String max = maxValue.givenMaxValue("black","cream","yellow");
+        Assert.assertEquals("yellow",max);
     }
 
 }
