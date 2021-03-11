3304a05f8c38edaff3cf0d3f89868e0eb30b0909
G.Sathwika <gopirajsathwika1999@gmail.com>
2021-03-11 21:01:05 +0530

    Refactored the code by using constructor

--- src/main/java/com/bridgelabz/MaxValue.java
+++ src/main/java/com/bridgelabz/MaxValue.java
@@ -1,6 +1,37 @@
 package com.bridgelabz;
 
-public class MaxValue {
+public class MaxValue<T> {
+     private T x, y, z;
+     public void givenMaxValue(T x, T y, T z) {
+         this.x = x;
+         this.y = y;
+         this.z = z;
+     }
+
+    public void setX(T x) {
+        this.x = x;
+    }
+
+    public void setY(T y) {
+        this.y = y;
+    }
+
+    public void setZ(T z) {
+        this.z = z;
+    }
+
+    public T getX() {
+        return x;
+    }
+
+    public T getY() {
+        return y;
+    }
+
+    public T getZ() {
+        return z;
+    }
+
     public static <T extends Comparable<T>> T givenMaxValue(T x, T y, T z) {
         if(x.compareTo(y) > 0) {
             if(x.compareTo(z) > 0) {
--- src/test/java/com/bridgelabz/MaxValueTest.java
+++ src/test/java/com/bridgelabz/MaxValueTest.java
@@ -5,62 +5,21 @@ import org.junit.Test;
 
 public  class MaxValueTest {
     @Test
-    public void when_first_position_is_greater_should_return_true() {
+    public void when_three_integers_given_return_max_value() {
         MaxValue maxValue = new MaxValue();
         int max = maxValue.givenMaxValue(5,1,3);
         Assert.assertEquals(5, max);
     }
-
     @Test
-    public void when_second_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        int max = maxValue.givenMaxValue(2,8,3);
-        Assert.assertEquals(8,max);
-    }
-    @Test
-    public void when_third_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        int max = maxValue.givenMaxValue(2,3,9);
-        Assert.assertEquals(9,max);
-    }
-
-    @Test
-    public void when_first_position_Float_is_greater_should_return_true() {
+    public void when_three_Floats_given_return_max_value() {
         MaxValue maxValue = new MaxValue();
         Float max = maxValue.givenMaxValue(5f,1f,3f);
         Assert.assertEquals(5f, max,0f);
     }
-
     @Test
-    public void when_second_Float_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        Float max = maxValue.givenMaxValue(2f,3f,1f);
-        Assert.assertEquals(3f,max,0f);
-    }
-    @Test
-    public void when_third_Float_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        Float max = maxValue.givenMaxValue(3f,4f,5f);
-        Assert.assertEquals(5f,max,0f);
-    }
-    @Test
-    public void when_first_position_String_is_greater_should_return_true() {
+    public void when_three_Strings_given_should_return_max_value() {
         MaxValue maxValue = new MaxValue();
         String max = maxValue.givenMaxValue("red","green","orange");
         Assert.assertEquals("red",max);
     }
-
-    @Test
-    public void when_second_String_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        String max = maxValue.givenMaxValue("pink","white","purple");
-        Assert.assertEquals("white",max);
-    }
-    @Test
-    public void when_third_String_position_is_greater_should_return_true() {
-        MaxValue maxValue = new MaxValue();
-        String max = maxValue.givenMaxValue("black","cream","yellow");
-        Assert.assertEquals("yellow",max);
-    }
-
 }
