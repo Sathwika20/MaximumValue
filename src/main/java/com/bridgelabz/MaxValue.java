6293c6a4d6f735d11bda0755c1c49600c06673d1
G.Sathwika <gopirajsathwika1999@gmail.com>
2021-03-11 16:51:05 +0530

    Added MaximumValue using Integer Added MaxValue using Float Added MaxValue using String Refactored the code to one Generic method

--- src/main/java/com/bridgelabz/MaxValue.java
+++ src/main/java/com/bridgelabz/MaxValue.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 public class MaxValue {
-    public static Integer givenMaxValue(Integer x, Integer y, Integer z) {
+    public static <T extends Comparable<T>> T givenMaxValue(T x, T y, T z) {
         if(x.compareTo(y) > 0) {
             if(x.compareTo(z) > 0) {
                 return (x);
@@ -14,32 +14,5 @@ public class MaxValue {
             return (z);
 
     }
-    public static Float givenMaxValue(Float x, Float y, Float z) {
-        if(x.compareTo(y) > 0) {
-            if(x.compareTo(z) > 0) {
-                return (x);
-            }  else {
-                return (z);
-            }
-        } else if (y.compareTo(z) > 0) {
-            return (y);
-        } else
-            return (z);
-
-    }
-    public static String givenMaxValue(String x, String y, String z) {
-        if(x.compareTo(y) > 0) {
-            if(x.compareTo(z) > 0) {
-                return (x);
-            }  else {
-                return (z);
-            }
-        } else if (y.compareTo(z) > 0) {
-            return (y);
-        } else
-            return (z);
-
-    }
-
 
 }
